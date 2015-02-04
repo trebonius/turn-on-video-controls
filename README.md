468a5792e6244af6c8e1810c78bad016a04be7a9
Tobin <bentobin@406c8f29f342.ant.amazon.com>
2015-02-04 12:10:44 -0800

    Add the script, update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # turn-on-video-controls
 A TamperMonkey/GreaseMonkey script to turn on video controls for all HTML5 videos.
+
+It's just something I slapped together. Let me know if you think there's a better way. You're probably right. 
+
+It works for me as of Feb 4, 2015 with Chrome on a Mac. Haven't actually tried with GreaseMonkey or other browsers yet.
---
+++ Turn On All HTML5 Video Controls.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Turn On All HTML5 Video Controls
+// @namespace    http://bentobin.com/videocontrols
+// @version      0.1
+// @description  Turns on all HTML5 video controls
+// @author       Ben Tobin
+// @match        http://*/*
+// @match		 https://*/*
+// @grant        none
+// ==/UserScript==
+
+// Setup a new observer to get notified of changes
+var observer = new MutationObserver(function (mutations) {
+    var DEBUG = false;
+    
+    mutations.forEach(function (mutation) {
+      
+      var entry = {
+        mutation: mutation,
+        el: mutation.target,
+        value: mutation.target.textContent,
+        oldValue: mutation.oldValue
+      };
+      if (DEBUG) console.log('Recording mutation:', entry);
+      if (mutation.type == "childList") {
+          for (var x = 0; x < mutation.addedNodes.length; x++)
+          {
+              var newNode = mutation.addedNodes[x];
+              if (DEBUG) console.log("AddedNode: ", newNode);
+              // Video added directly
+              if (newNode.tagName == "VIDEO") {
+                  if (DEBUG) console.log("Found a video!: ", video);
+                  turnOnControls(newNode);
+              // Some nodes (text) don't let me call this, so don't try.
+              // TODO: Figure out if we might be missing video tags because of this
+              } else if (newNode.getElementsByTagName) {
+                  // Video added as child
+                  var videos = newNode.getElementsByTagName("VIDEO");
+                  for (var y = 0; y < videos.length; y++) {
+                      var video = videos[y];
+                      if (DEBUG) console.log("Found a video!: ", video);
+                      turnOnControls(video);
+                  }
+              }
+          }
+      }
+    });
+});
+
+// What we're all here for
+function turnOnControls(video) {
+	video.setAttribute("controls","controls");
+}
+
+var options = {
+  subtree: true,
+  childList: true,
+  attributes: false
+};
+
+// Observe a specific DOM node / subtree
+observer.observe(document, options);
